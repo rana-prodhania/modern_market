4dc62f355ba95ae64e5d0b3243de893da620946a
MD Rana Prodhania <ranaprodhania@gmail.com>
2023-05-27 08:22:44 +0600

    Improved errorMiddleware mongodb castError code

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,13 +1,11 @@
-import createError from "http-errors";
-
 const errorMiddleware = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.message = error.message || "Internal Server Error";
 
   // Wrong Mongodb Id error
   if (error.name === "CastError") {
-    const message = `Resource not found. Invalid: ${error.path}`;
-    error = new createError(400, message);
+    error.statusCode = 404;
+    error.message = "Invalid Id and the Resource is not found";
   }
 
   res.status(error.statusCode).json({
