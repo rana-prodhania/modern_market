d72423d5dbb021aa6081513da0ee0f190dd1b674
MD Rana Prodhania <ranaprodhania@gmail.com>
2023-05-29 22:14:35 +0600

    fix: duplicate, jwt error with better msg

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,6 +8,24 @@ const errorMiddleware = (error, req, res, next) => {
     error.message = "Invalid Id and the Resource is not found";
   }
 
+  // Mongoose duplicate key error
+  if (error.code === 11000) {
+    error.statusCode = 400;
+    error.message = `Duplicate ${Object.keys(error.keyValue)} value entered`;
+  }
+
+  // Wrong JWT error
+  if (error.name === "JsonWebTokenError") {
+    error.statusCode = 401;
+    error.message = "Invalid token";
+  }
+
+  // JWT expired error
+  if (error.name === "TokenExpiredError") {
+    error.statusCode = 401;
+    error.message = "Token expired";
+  }
+
   res.status(error.statusCode).json({
     success: false,
     message: error.message,
